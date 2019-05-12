d31a7798f378c875701ad1d816d48b07e14e1d0a
marguello <majusotela@gmail.com>
2019-05-11 21:33:35 -0600

    trabajo semana 2

---
+++ tarea_1.py
@@ -0,0 +1,4 @@
+Pi = 3.1415926
+stringValue = "*********"
+
+print("%.1s" %(stringValue)," %.7f" % Pi , " %.2s" %(stringValue)," %.6f" % Pi, "%.3s" %(stringValue)," %.5f" % Pi," %.4s" %(stringValue)," %.4f" % Pi, " %.5s" %(stringValue)," %.3f" % Pi," %.6s" %(stringValue)," %.2f" % Pi )
