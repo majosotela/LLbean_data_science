5a378021438d30d803489e3691cea5922f18db1c
marguello <majusotela@gmail.com>
2019-05-13 21:09:04 -0600

    primera parte de la tarea 3

--- tarea_3.py
+++ tarea_3.py
@@ -1,15 +1,16 @@
-import operator
+from operator import itemgetter, attrgetter
+from collections import Counter
 
 # Para definir la agenda del hospital
 schedule_ER = []
 
-user_list =[(('Juan', 'Mora', 100103111,65 , 81118811, 'dolor'),
+user_list =(('Juan', 'Mora', 100103111,65 , 81118811, 'dolor'),
           ('Ana', 'Jimenez', 32415116,50 , 46261266, 'consulta'),
           ('Sofia',   'Alfaro',   32415116,   36 , 51161161, 'consulta'),
           ('Carlos',  'Sanchez',  33411151,   15 , 41655161, 'dolor'),
           ('Felipe',  'Perez',    12243151,   42 , 65151111, 'documento'),
           ('Melissa', 'Alvarado', 734114144,  10 , 87421312, 'dolor'),
-          ('Pedro',   'Castro',   4372124141, 2 ,  99313131, 'dolor'))]
+          ('Pedro',   'Castro',   4372124141, 2 ,  99313131, 'dolor'))
 
 
 schedule_ER.append(user_list)
@@ -18,30 +19,52 @@ schedule_ER.append(user_list)
 Primera pregunta:
 Cuantos pacientes llegaron en total?
 '''
-user_count=0
-for patient_count in user_list:
-      for p in patient_count:
-          user_count=user_count+1
+user_count= len(user_list)
 print ('1.Total de personas atendidas: ', user_count)
 
 '''
 Segunda pregunta:
 Cuantas personas llegaron por dolor?
 '''
-count = user_list.count(('dolor'))
-print ("La cantidad de usuarios que llegaron con dolor son: ", count)
+counter = 0
+for user in user_list:
+    if 'dolor' in user:
+       counter += 1
 
-
+print("2.El numero de personas que llegaron por dolor es: ", counter)
 """
 Cuarta pregunta
 Suponga que se atienden con orden de prioridad por edad, empezando por el adulto mayor. Ordene la lista desde el más adulto al más joven
 
 """
+print('3.La lista de pacientes empezando por el paciente de mayor edad al menor',(sorted(user_list, key =itemgetter(3), reverse=True)))
 
-schedule_ER.append(user_list)
+'''
+Cuantos pacientes son mayores de edad? cuantos menores?
 
-print('4.La lista ordenada desde el paciente de mayor edad al menor: ',sorted(user_list, key=operator.itemgetter(3)))
+'''
+
+print( '4. Cuantos pacientes son mayores de edad?',len([x for x in user_list if x[3] > 18]))
+print( '4. Cuantos pacientes son menores de edad?', len([x for x in user_list if x[3] < 18]))
+
+
+'''Suponga que se atienden con orden de prioridad por gravedad de consulta, 
+empezando por los que tienen dolor y luego por edad (mas viejo al joven), empezando por el adulto mayor. 
+Ordene la lista empenzando por los que tienen mayor prioridad.
+
+'''
+print ('5. Lista de pacientes segun su prioridad,siendo esta los que tienen dolor y luego por edad: ',sorted(user_list, key=itemgetter(5,3), reverse=True))
+
+
+'''
+Suponga que los que tienen dolor mueren :( 
+Como queda la lista de pacientes vivos por atender ordenados por orden de edad desde el joven al viejo.
+
+'''
+
+
+print('La nueva lista de prioridad seria: ',sorted(user_list, key=itemgetter(5)),len([x for x in user_list if x[5] != 'dolor']))
 
 
 
-
+print(sorted(user_list, key=itemgetter(3), reverse=True))
